0a100c919d0bd5a67ff8678ddfe70492f8177a08
Carlo Contardi <c.contardi@icloud.com>
2022-10-02 17:22:08 +0200

    Model added

---
+++ src/View/Finestra.java
@@ -0,0 +1,12 @@
+package View;
+
+import javax.swing.*;
+
+public class Finestra {
+    private JPanel panel1;
+    private JButton aggiungiButton;
+    private JButton modificaButton;
+    private JButton rimuoviButton;
+    private JTable tabellaRubrica;
+    private JButton importaButton;
+}
